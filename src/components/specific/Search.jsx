20ee3ea6c3713e078bde5634c8a4251eae21c9ba
shubhanshu <shubhanshumohanty@gmail.com>
2024-11-11 15:37:50 +0530

    add useMutation hook

--- src/components/specific/Search.jsx
+++ src/components/specific/Search.jsx
@@ -1,33 +1,35 @@
-import React, { useEffect, useState } from "react";
-import toast from "react-hot-toast";
+import { useInputValidation } from "6pp";
+import { Search as SearchIcon } from "@mui/icons-material";
 import {
   Dialog,
   DialogTitle,
   InputAdornment,
-  Stack,
-  TextField,
-  Typography,
   List,
-  ListItem,
-  ListItemText,
+  Stack,
+  TextField
 } from "@mui/material";
-import { Search as SearchIcon } from "@mui/icons-material";
-import { useInputValidation } from "6pp";
-import UserItem from "../shared/UserItem";
-import { sampleUsers } from "../../constants/sampleData";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useAsyncMutation } from "../../hooks/hook";
+import {
+  useLazySearchUserQuery,
+  useSendFriendRequestMutation,
+} from "../../redux/api/api";
 import { setIsSearch } from "../../redux/reducers/misc";
-import { useLazySearchUserQuery, useSendFriendRequestMutation } from "../../redux/api/api";
+import UserItem from "../shared/UserItem";
 const Search = () => {
   const { isSearch } = useSelector((state) => state.misc);
-  const [searchUser]=useLazySearchUserQuery()
-  const [sendFriendRequest]=useSendFriendRequestMutation();
+  const [searchUser] = useLazySearchUserQuery();
+  const [sendFriendRequest, isLoadingSendFriendRequest] = useAsyncMutation(
+    useSendFriendRequestMutation
+  );
   const search = useInputValidation("");
   const [users, setUsers] = useState([]);
   const dispatch = useDispatch();
 
-  const addFriendHandler = async(id)=> {
+  const addFriendHandler = async (id) => {
     // Add friend logic here
+    /*
     try {
       const res=await sendFriendRequest({userId: id})
       if(res.data){
@@ -40,22 +42,23 @@ const Search = () => {
       }
     } catch (error) {
       toast.error(error.message);
-    }
+    }*/
+    await sendFriendRequest("Sending friend request...", { userId: id });
   };
-  const isLoadingSendFriendRequest=()=>{
-    console.log("isLoadingSendFriendRequest");
-  }
-  useEffect(()=>{
-    const timeOutId=setTimeout(()=>{
-      searchUser(search.value)
-      .then(({data})=>setUsers(data.users))
-      .catch((e)=>console.log(e))
-    },1000)
-    return ()=>{
-      clearTimeout(timeOutId)
-    }
 
-  },[search.value])
+  /*const isLoadingSendFriendRequest=()=>{
+    console.log("isLoadingSendFriendRequest");
+  }*/
+  useEffect(() => {
+    const timeOutId = setTimeout(() => {
+      searchUser(search.value)
+        .then(({ data }) => setUsers(data.users))
+        .catch((e) => console.log(e));
+    }, 1000);
+    return () => {
+      clearTimeout(timeOutId);
+    };
+  }, [search.value]);
   const searchCloseHandler = () => dispatch(setIsSearch(false));
   return (
     <Dialog open={isSearch} onClose={searchCloseHandler}>

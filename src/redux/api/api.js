20a19edc176a9ea98fbce63a305435560b4913c3
shubhanshu <shubhanshumohanty@gmail.com>
2024-11-11 14:24:20 +0530

    please ignore this commit i just did  fromate

--- src/redux/api/api.js
+++ src/redux/api/api.js
@@ -1,29 +1,27 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { server } from "../../constants/config";
 
-const api=createApi({
-    reducerPath:"api",
-    baseQuery:fetchBaseQuery({baseUrl:`${server}/api/v1/`}),
-    tagTypes:["Chat","User"],
-    endpoints:(builder)=>({
-        myChats:builder.query(({
-            query:()=>({
-                url:"chat/my",
-                credentials:"include",
-            }),
-            providesTags:["Chat"]
-        })),
-        searchUser: builder.query({
-            query: (name) => ({
-              url: `user/search?name=${name}`,
-              credentials: "include",
-            }),
-            providesTags: ["User"],
-          }),
-    })
+const api = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: `${server}/api/v1/` }),
+  tagTypes: ["Chat", "User"],
+  endpoints: (builder) => ({
+    myChats: builder.query({
+      query: () => ({
+        url: "chat/my",
+        credentials: "include",
+      }),
+      providesTags: ["Chat"],
+    }),
+    searchUser: builder.query({
+      query: (name) => ({
+        url: `user/search?name=${name}`,
+        credentials: "include",
+      }),
+      providesTags: ["User"],
+    }),
+  }),
 });
 
 export default api;
-export const {
-    useMyChatsQuery,useLazySearchUserQuery
-}=api;
+export const { useMyChatsQuery, useLazySearchUserQuery } = api;

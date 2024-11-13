cbd20f3cb0dddc2e8c30e39db2a9a4a54c8ac6ef
shubhanshu <shubhanshumohanty@gmail.com>
2024-11-13 15:28:44 +0530

    wrap some route in socket provider

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { server } from "./constants/config";
 import { useDispatch, useSelector } from "react-redux";
 import { userExists, userNotExists } from "./redux/reducers/auth";
 import { Toaster } from "react-hot-toast";
+import { SocketProvider } from "./socket";
 const Home = lazy(() => import("./pages/Home"));
 const Login = lazy(() => import("./pages/Login"));
 const Chat = lazy(() => import("./pages/Chat"));
@@ -45,7 +46,9 @@ function App() {
                 <Home/>
                 </ProtectRoute>}/> */}
 
-          <Route path="/" element={<ProtectRoute user={user} />}>
+          <Route path="/" element={<SocketProvider>
+                <ProtectRoute user={user} />
+              </SocketProvider>}>
             <Route path="/" element={<Home />} />
             <Route path="/chat/:chatId" element={<Chat />} />
             <Route path="/groups" element={<Groups />} />
